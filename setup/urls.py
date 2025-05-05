8519f5f5096b3628efd51543cb55c93a05fe06b6
Rafaellegend <Rafaellegendofdark@hotmail.com>
2025-05-05 20:35:06 -0300

    feat: adding api router

--- setup/urls.py
+++ setup/urls.py
@@ -1,7 +1,14 @@
 from django.contrib import admin
 from django.urls import include, path
+from rest_framework import routers
+from vendas.views import CategoriaViewSet, ProdutoViewSet
+
+router = routers.DefaultRouter()
+router.register('produtos', ProdutoViewSet, basename='Produto')
+router.register('categorias', CategoriaViewSet, basename='Categoria')
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api-auth/', include('rest_framework.urls')),
+    path('',include(router.urls))
 ]
