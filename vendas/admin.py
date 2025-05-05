4c637705bee17cf0f99ad8c2a71d9e3ab00f7f44
Rafaellegend <Rafaellegendofdark@hotmail.com>
2025-05-05 20:27:15 -0300

    feat: setting admin view

--- vendas/admin.py
+++ vendas/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from vendas.models import Produto, Categoria
 
-# Register your models here.
+class Produtos(admin.ModelAdmin):
+    list_display = ('nome', 'preco', 'quantidade_estoque', 'categoria', 'criado_em', 'atualizado_em',)
+    list_display_links = ('nome',)
+    list_per_page = 20
+    search_fields = ('nome', 'categoria__nome')
+    ordering = ('nome','-criado_em',)
+
+admin.site.register(Produto, Produtos)
+
+class Categorias(admin.ModelAdmin):
+    list_display = ('nome', 'descricao', 'criado_em', 'atualizado_em',)
+    list_display_links = ('nome',)
+    list_per_page = 20
+    search_fields = ('nome',)
+    ordering = ('nome', '-criado_em',)
+
+admin.site.register(Categoria, Categorias)
